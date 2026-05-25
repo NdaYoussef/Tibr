e5ac84a89ce789bba76446feb687df676af863f0
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-05-25 20:40:34 +0300

    Add generic repository interface CRUD methods

--- Tibr.Domain/IRepositories/IGenericRepository.cs
+++ Tibr.Domain/IRepositories/IGenericRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tibr.Domain.Common.Classes;
 
 namespace Tibr.Domain.IRepositories
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<TEntity,TId> where TEntity : BaseEntity<TId>
+
     {
+        public Task<IQueryable<TEntity>> GetAll();
+        public Task AddAsync(TEntity entity);
+        public Task UpdateAsync(TEntity entity);
+        public Task DeleteAsync(TEntity entity);
+        public Task<int> SaveChangesAsync();
     }
 }
