e65ad00e8643d92a702b1c682fb108412b1979c4
NdaMousa <ndayou99@gmail.com>
2026-05-28 17:57:58 +0300

    add support services implementation

--- Tibr.Application/Services/SupportServices/SupportService.cs
+++ Tibr.Application/Services/SupportServices/SupportService.cs
@@ -1,42 +1,97 @@
-﻿using System;
+﻿using Mapster;
+using MapsterMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Tibr.Application.Dtos.SupportDtos;
 using Tibr.Application.Services.SupportServices;
+using Tibr.Domain.Entities;
+using Tibr.Domain.IRepositories;
 using Tibr.Domain.ResultPattern;
 
 namespace Tibr.Application.Services.SuppoertServices
 {
     public class SupportService : ISupportService
     {
-
-        public SupportService()
+        private readonly ISupportRepository _supportRepository;
+        private readonly IMapper _mapper;
+       
+        public SupportService(ISupportRepository supportRepository, IMapper mapper)
         {
+            _supportRepository = supportRepository;
+            _mapper = mapper;
+        }
+        public async Task<Result<string>> AddSupportAsync(CreateSupportRequestDto createSupportRequestDto0)
+        {
+            var support = _mapper.Map<Support>(createSupportRequestDto0);
+            await _supportRepository.AddAsync(support);
             
-        }
-        public Task<Result> AddSupportAsync(CreateSupportRequestDto createSupportRequestDto0)
-        {
-            throw new NotImplementedException();
+            var result = await _supportRepository.SaveChangesAsync();
+            if (result <= 0)
+            {
+                return Result<string>.Failure("Failed to add support request.");
+            }
+
+            return "Support request added successfully";
         }
 
-        public Task<Result> DeleteSupportAsync(long id)
+        public async Task<Result<string>> DeleteSupportAsync(long id)
         {
-            throw new NotImplementedException();
+            var support = await _supportRepository.GetById(id);
+            if (support == null)
+            {
+                return Result<string>.Failure("Support request not found.");
+            }
+
+            await _supportRepository.DeleteAsync(support);
+            var result = await _supportRepository.SaveChangesAsync();
+            if (result <= 0)
+            {
+                return Result<string>.Failure("Failed to delete support request.");
+            }
+
+            return Result<string>.Success("Support request deleted successfully.");
         }
 
-        public Task<Result<List<SupportResponse>>> GetAllSupportsAsync()
+        public async Task<Result<List<SupportResponse>>> GetAllSupportsAsync()
         {
-            throw new NotImplementedException();
+            var supportsEntities = await _supportRepository.GetAllAsync();
+
+            var supportResponses = supportsEntities.Adapt<List<SupportResponse>>();
+
+            return Result<List<SupportResponse>>.Success(supportResponses);
         }
 
-        public Task<Result<SupportResponse>> GetSupportByIdAsync(long id)
+        public async Task<Result<SupportResponse>> GetSupportByIdAsync(long id)
         {
-            throw new NotImplementedException();
+            var support = await _supportRepository.GetById(id);
+            if(support == null)
+            {
+                return Result<SupportResponse>.Failure("Support request not found.");
+            }
+
+            var supportResponse = support.Adapt<SupportResponse>();
+            return Result<SupportResponse>.Success(supportResponse);
         }
 
-        public Task<Result> UpdateSupportAsync(UpdateSupportDto updateSupportDto)
+        public async Task<Result<string>> UpdateSupportAsync(UpdateSupportDto updateSupportDto)
         {
-            throw new NotImplementedException();
+            if(updateSupportDto == null)
+            {
+                return Result<string>.Failure("Invalid support request data.");
+            }
+            var support = _mapper.Map<Support>(updateSupportDto);
+            
+            await _supportRepository.UpdateAsync(support);
+            
+            var result = await _supportRepository.SaveChangesAsync();
+            if (result <= 0)
+            {
+                return Result<string>.Failure("Failed to update support request.");
+            }
+
+            return Result<string>.Success("Support request updated successfully.");
         }
     }
 }
