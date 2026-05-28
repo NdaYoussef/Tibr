b71d18cf19798dde2346ed54b1aac6ce49687831
NdaMousa <ndayou99@gmail.com>
2026-05-28 16:58:50 +0300

    add support services

--- Tibr.Application/Services/SupportServices/SupportService.cs
+++ Tibr.Application/Services/SupportServices/SupportService.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tibr.Application.Dtos.SupportDtos;
+using Tibr.Application.Services.SupportServices;
+using Tibr.Domain.ResultPattern;
 
 namespace Tibr.Application.Services.SuppoertServices
 {
-    public class SupportService
+    public class SupportService : ISupportService
     {
+
+        public SupportService()
+        {
+            
+        }
+        public Task<Result> AddSupportAsync(CreateSupportRequestDto createSupportRequestDto0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> DeleteSupportAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<List<SupportResponse>>> GetAllSupportsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<SupportResponse>> GetSupportByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdateSupportAsync(UpdateSupportDto updateSupportDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
