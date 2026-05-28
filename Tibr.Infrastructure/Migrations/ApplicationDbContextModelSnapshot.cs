a8a2807b74da3eb8aefb8078d369588609dce1c4
NdaMousa <ndayou99@gmail.com>
2026-05-28 15:49:03 +0300

    add first migration

--- Tibr.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Tibr.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -496,7 +496,7 @@ namespace Tibr.Infrastructure.Migrations
                     b.ToTable("Products");
                 });
 
-            modelBuilder.Entity("Tibr.Domain.Entities.SupportTicket", b =>
+            modelBuilder.Entity("Tibr.Domain.Entities.Support", b =>
                 {
                     b.Property<long>("Id")
                         .ValueGeneratedOnAdd()
@@ -531,7 +531,7 @@ namespace Tibr.Infrastructure.Migrations
                     b.ToTable("SupportTickets");
                 });
 
-            modelBuilder.Entity("Tibr.Domain.Entities.TicketReply", b =>
+            modelBuilder.Entity("Tibr.Domain.Entities.Ticket", b =>
                 {
                     b.Property<long>("Id")
                         .ValueGeneratedOnAdd()
@@ -770,7 +770,7 @@ namespace Tibr.Infrastructure.Migrations
                     b.Navigation("Category");
                 });
 
-            modelBuilder.Entity("Tibr.Domain.Entities.SupportTicket", b =>
+            modelBuilder.Entity("Tibr.Domain.Entities.Support", b =>
                 {
                     b.HasOne("Tibr.Domain.Entities.User", "User")
                         .WithMany("SupportTickets")
@@ -781,7 +781,7 @@ namespace Tibr.Infrastructure.Migrations
                     b.Navigation("User");
                 });
 
-            modelBuilder.Entity("Tibr.Domain.Entities.TicketReply", b =>
+            modelBuilder.Entity("Tibr.Domain.Entities.Ticket", b =>
                 {
                     b.HasOne("Tibr.Domain.Entities.Admin", "Admin")
                         .WithMany("TicketReplies")
@@ -789,15 +789,15 @@ namespace Tibr.Infrastructure.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("Tibr.Domain.Entities.SupportTicket", "Ticket")
-                        .WithMany("TicketReplies")
+                    b.HasOne("Tibr.Domain.Entities.Support", "Support")
+                        .WithMany("Tickets")
                         .HasForeignKey("TicketId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
                     b.Navigation("Admin");
 
-                    b.Navigation("Ticket");
+                    b.Navigation("Support");
                 });
 
             modelBuilder.Entity("Tibr.Domain.Entities.Admin", b =>
@@ -835,9 +835,9 @@ namespace Tibr.Infrastructure.Migrations
                     b.Navigation("OrderItems");
                 });
 
-            modelBuilder.Entity("Tibr.Domain.Entities.SupportTicket", b =>
+            modelBuilder.Entity("Tibr.Domain.Entities.Support", b =>
                 {
-                    b.Navigation("TicketReplies");
+                    b.Navigation("Tickets");
                 });
 
             modelBuilder.Entity("Tibr.Domain.Entities.User", b =>
