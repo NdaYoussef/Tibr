bedb132956a99dac60ec580f4355eee522a2f747
NdaMousa <ndayou99@gmail.com>
2026-05-28 16:41:07 +0300

    add support dtos

---
+++ Tibr.Application/Dtos/SupportDtos/UpdateSupportDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Tibr.Application.Dtos.SupportDtos
+{
+    public class UpdateSupportDto
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Status cannot exceed 50 characters.")]
+        public string Status { get; set; } = string.Empty;
+    }
+}
