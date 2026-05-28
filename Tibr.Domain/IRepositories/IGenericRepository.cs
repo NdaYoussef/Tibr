d0b1cc0e46ba4f0d2fd8c6858f787ed43815ca08
NdaMousa <ndayou99@gmail.com>
2026-05-28 17:58:36 +0300

    handle git all generic method

--- Tibr.Domain/IRepositories/IGenericRepository.cs
+++ Tibr.Domain/IRepositories/IGenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using Tibr.Domain.Common.Classes;
 
@@ -7,12 +8,13 @@ namespace Tibr.Domain.IRepositories
 {
     public interface IGenericRepository<TEntity, TId> where TEntity : BaseEntity<TId>
     {
-         IQueryable<TEntity> GetAll();
-         Task<TEntity?> GetById(TId id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetById(TId id);
          Task AddAsync(TEntity entity);
         Task UpdateAsync(TEntity entity);
         Task DeleteAsync(TEntity entity);
         Task<int> SaveChangesAsync();
+         IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
     }
     }
 
