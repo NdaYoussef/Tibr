268d3c441b9f6f9b71393530c5e19f29b48aff43
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-05-29 19:01:57 +0300

    Add category service interface and implementation

---
+++ Tibr.Application/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tibr.Application.Dtos.CategoryDto;
+using Tibr.Domain.ResultPattern;
+
+namespace Tibr.Application.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<Result<IEnumerable<CategoryDto>>> GetAllCategoriesAsync();
+        Task<Result<CategoryDto>> GetCategoryByIdAsync(long id);
+        Task<Result<CategoryDto>> AddCategoryAsync(CreateCategoryDto dto);
+        Task<Result<CategoryDto>> UpdateCategoryAsync(long id, UpdateCategoryDto dto);
+        Task<Result> DeleteCategoryAsync(long id);
+    }
+}
