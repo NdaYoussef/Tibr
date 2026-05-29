94cbb7798dde911c96e97fc6161994def3483b16
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-05-29 18:00:02 +0300

    Configure mapping for category Dtos

--- Tibr.Application/Mappers/CategoryMappingConfig.cs
+++ Tibr.Application/Mappers/CategoryMappingConfig.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Mapster;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Tibr.Application.Dtos.CategoryDto;
+using Tibr.Domain.Entities;
 
 namespace Tibr.Application.Mappers
 {
-    internal class CategoryMappingConfig
+    public class CategoryMappingConfig : IRegister
     {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Category, CategoryDto>()
+                .Map(dest => dest.ProductCount, src =>
+                    src.Products != null ? src.Products.Count : 0);
+
+            config.NewConfig<CreateCategoryDto, Category>()
+                .Ignore(dest => dest.Id)
+                .Ignore(dest => dest.Products)
+                .Ignore(dest => dest.IsDeleted)
+                .Ignore(dest => dest.CreatedAt);
+
+            config.NewConfig<UpdateCategoryDto, Category>()
+                .Ignore(dest => dest.Id)
+                .Ignore(dest => dest.Products)
+                .Ignore(dest => dest.IsDeleted)
+                .Ignore(dest => dest.CreatedAt);
+        }
     }
+
 }
