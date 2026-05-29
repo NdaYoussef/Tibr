b4123759a1189a8a572e31325e716bae6b5f281b
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-05-29 18:27:28 +0300

    Add category repository interface and implementation methods

---
+++ Tibr.Domain/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tibr.Domain.Entities;
+
+namespace Tibr.Domain.IRepositories
+{
+    public interface ICategoryRepository : IGenericRepository<Category, long>
+    {
+        Task<bool>GetByNameAsync(string name);
+    }
+}
---
+++ Tibr.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tibr.Domain.Entities;
+using Tibr.Domain.IRepositories;
+using Tibr.Infrastructure.Contexts;
+
+namespace Tibr.Infrastructure.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category, long>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> GetByNameAsync(string name)
+        {
+            return await _context.Categories
+                .AnyAsync(c => c.Name.ToLower() == name.ToLower()
+                            && !c.IsDeleted);
+        }
+    }
+
+}
