ff8ba802c2cef738eeb75ad72a8300c5749f3ca6
Eslam Abd-Elsatar <eslamlegend5@gmail.com>
2026-06-05 17:56:43 +0300

    step 1: create IPaymentGateway interface + provider-agnostic DTOs

---
+++ Tibr.Application/Services/PaymentServices/IPaymentGateway.cs
@@ -0,0 +1,10 @@
+using Tibr.Application.Dtos.Payment;
+
+namespace Tibr.Application.Services.PaymentServices;
+
+public interface IPaymentGateway
+{
+    Task<PaymentInitiationResult> CreateIntentionAsync(PaymentIntentionRequest request);
+    bool VerifyWebhook(string rawBody, string signature);
+    PaymentWebhookData ExtractWebhookData(string rawBody);
+}
