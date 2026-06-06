e58075b6efce1ee6dd7eb0e82b737156ffa5458e
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-06-07 00:23:50 +0300

    Add dashboard constants

---
+++ Tibr.MVC/Constants/ReportTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Tibr.MVC.Constants
+{
+    public static class ReportTypes
+    {
+        public const string Sales = "sales";
+        public const string Revenue = "revenue";
+        public const string Products = "products";
+        public const string Customers = "customers";
+        public const string Inventory = "inventory";
+    }
+}
