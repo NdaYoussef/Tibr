bfc06650df2293231c218cf3e4565fd16c6bee79
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-06-07 00:36:11 +0300

    Add product controller

---
+++ Tibr.MVC/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tibr.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
