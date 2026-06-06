755eb2a088a719f76c7aa208d95287dc07669504
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-06-07 00:27:47 +0300

    Add analytics controller

---
+++ Tibr.MVC/Controllers/AnalyticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tibr.MVC.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
