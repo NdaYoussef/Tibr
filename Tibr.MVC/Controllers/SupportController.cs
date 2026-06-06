b9b5af53786363408d384c55ec58ba11e4f8ac60
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-06-07 00:37:11 +0300

    Add support controller

---
+++ Tibr.MVC/Controllers/SupportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tibr.MVC.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
