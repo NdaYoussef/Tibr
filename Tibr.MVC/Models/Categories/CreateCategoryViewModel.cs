408f290202bb03e749933fe3d87f458ebe4d417b
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-06-07 07:17:09 +0300

    Create category view models

---
+++ Tibr.MVC/Models/Categories/CreateCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tibr.MVC.Models.Categories
+{
+
+    public class CreateCategoryViewModel
+    {
+        [Required(ErrorMessage = "Category name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters")]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
