4a96b46aaf01fc8f8f7b84c62f1facbc94541d02
afafrady22 <afafradi499@gmail.com>
2026-06-17 15:14:44 +0300

    update user and order

--- Tibr.Infrastructure/Migrations/20260617113146_initialCreated.cs
+++ Tibr.Infrastructure/Migrations/20260617113146_initialCreated.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Tibr.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class initialCreated : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
